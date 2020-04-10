bdce29223a752088ec3ea68835f8f348e5579dbb
A.K.A 009 <zhangchang_000@163.com>
2020-04-10 14:26:41 +0800

    first commit

---
+++ Assets/PackageTest/Runtime/TestRuntimeAPI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Packages.PackageTest.Runtime
+{
+    public class TestRuntimeAPI
+    {
+        public int GetTestNumber()
+        {
+            return 666;
+        }
+    }
+}
