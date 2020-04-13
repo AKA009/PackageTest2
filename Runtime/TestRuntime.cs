9dc8b8f266def3795ea7c6bfb6714b0b7ed0a990
A.K.A 009 <zhangchang_000@163.com>
2020-04-13 15:35:47 +0800

    Reset project

---
+++ Runtime/TestRuntime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Packages.PackageTest.Runtime
+{
+    public class TestRuntime: MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("TestRuntimeMonoBehaviour");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
